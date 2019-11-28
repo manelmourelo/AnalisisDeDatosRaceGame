4acb4afb29ef3c5aaada13e0fa83233ab344121b
manelmourelo <manelmourelo.1@gmail.com>
2019-11-28 17:03:20 +0100

    Save laps.csv

--- RaceGame/Assets/_Scripts/EventsHandler.cs
+++ RaceGame/Assets/_Scripts/EventsHandler.cs
@@ -9,6 +9,10 @@ public class EventsHandler : MonoBehaviour
 {
     private List<string[]> level_events_data = new List<string[]>();
 
+    private List<string[]> laps_data = new List<string[]>();
+
+    public string username = "manelmm3";
+
     enum TypeEvent
     {
         EVENT_NONE,
@@ -27,23 +31,23 @@ public class EventsHandler : MonoBehaviour
 
 
 
-        
+
 
         //string[] row_data_temp = new string[3];
 
 
         //string[] file = System.IO.File.ReadAllLines("Assets/CSV/level_events.csv"); 
 
-
-
-        if (System.IO.File.Exists("Assets/CSV/level_events.csv") == false)
+        if (System.IO.File.Exists("Assets/CSV/laps.csv") == false)
         {
-            string[] row_data_temp = new string[3];
+            string[] row_data_temp = new string[4];
 
-            row_data_temp[0] = "type";
-            row_data_temp[1] = "time";
-            row_data_temp[2] = "session_id";
-            level_events_data.Add(row_data_temp);
+            row_data_temp[0] = "lap_id";
+            row_data_temp[1] = "session_id";
+            row_data_temp[2] = "username";
+            row_data_temp[3] = "time";
+            laps_data.Add(row_data_temp);
+            Save(TypeEvent.LAP_DONE);
         }
 
         else
@@ -64,11 +68,14 @@ public class EventsHandler : MonoBehaviour
     public void WriteGoal(float time, int lap)
     {
 
-        string[] row_data_temp = new string[3];
-        row_data_temp[0] = "1";
-        row_data_temp[1] = time.ToString();
-        row_data_temp[2] = lap.ToString();
-        level_events_data.Add(row_data_temp);
+        PlayerPrefs.SetString("time", System.DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+
+        string[] row_data_temp = new string[4];
+        row_data_temp[0] = lap.ToString();
+        row_data_temp[1] = "0";
+        row_data_temp[2] = username;
+        row_data_temp[3] = PlayerPrefs.GetString("time");
+        laps_data.Add(row_data_temp);
         Save(TypeEvent.LAP_DONE);
 
     }
@@ -86,16 +93,16 @@ public class EventsHandler : MonoBehaviour
                 break;
             case TypeEvent.LAP_DONE:
 
-                path = "Assets/CSV/level_events.csv";
+                path = "Assets/CSV/laps.csv";
 
-                int length = level_events_data.Count;
+                int length = laps_data.Count;
                 string delimiter = ";";
 
 
 
 
                 for (int index = 0; index < length; index++) 
-                    sb.Append(string.Join(delimiter, level_events_data[index]));
+                    sb.Append(string.Join(delimiter, laps_data[index]));
                     
                    
         
@@ -109,6 +116,6 @@ public class EventsHandler : MonoBehaviour
         StreamWriter outStream = System.IO.File.AppendText(path);
         outStream.WriteLine(sb);
         outStream.Close();
-        level_events_data.Clear();
+        laps_data.Clear();
     }
 }
